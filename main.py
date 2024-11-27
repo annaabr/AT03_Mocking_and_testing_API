278a00581267dc5b9d3440ea42cc1c349b150a2b
annaabr <annaabramyan2013@gmail.com>
2024-11-27 21:46:33 +0300

    homework done

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import requests
 
-
-def get_weather(api_key, city):
-    url = f'[<http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}>](<http://api.openweathermap.org/data/2.5/weather?q=%7Bcity%7D&appid=%7Bapi_key%7D>)'
+def get_random_cat_image(url):
     response = requests.get(url)
     if response.status_code == 200:
-        return response.json()
+        data = response.json()  # Преобразование ответа в JSON
+        return data
     else:
         return None
 
-
-
+# Пример использования функции
+if __name__ == "__main__":
+    url = "https://api.thecatapi.com/v1/images/search"
+    cat_image = get_random_cat_image(url)
+    cat_image_url = cat_image[0]['url']  # Получение URL изображения
+    if cat_image:
+        print("Случайное изображение кошки:", cat_image)
--- test.py
+++ test.py
@@ -1,37 +1,37 @@
 import pytest
-from main import get_weather
+from main import get_random_cat_image
 
-def test_get_weather_success(mocker):
+def test_get_random_cat_image_success(mocker):
     mock_get = mocker.patch('main.requests.get')
     # Создаем мок-ответ для успешного запроса
     mock_get.return_value.status_code = 200
-    mock_get.return_value.json.return_value = {
-        'weather': [{'description': 'clear sky'}],
-        'main': {'temp': 273.15}
-    }
+    mock_get.return_value.json.return_value =  [{
+        'id': 'yKXeUMNZ8',
+        'url': 'https://cdn2.thecatapi.com/images/yKXeUMNZ8.jpg',
+        'width': 1080,
+        'height': 1350
+    }]
 
-    api_key = 'test_api_key'
-    city = 'London'
+    url = "https://api.thecatapi.com/v1/images/search"
+    cat_image_data = get_random_cat_image(url)
 
-    weather_data = get_weather(api_key, city)
+    assert cat_image_data == [{
+        'id': 'yKXeUMNZ8',
+        'url': 'https://cdn2.thecatapi.com/images/yKXeUMNZ8.jpg',
+        'width': 1080,
+        'height': 1350
+    }]
 
-    assert weather_data == {
-        'weather': [{'description': 'clear sky'}],
-        'main': {'temp': 273.15}
-    }
 
-def test_get_weather_with_error(mocker):
+
+
+def test_get_random_cat_image_with_error(mocker):
     mock_get = mocker.patch('main.requests.get')
     # Создаем мок-ответ для успешного запроса
     mock_get.return_value.status_code = 404
-    mock_get.return_value.json.return_value = {
-        'weather': [{'description': 'clear sky'}],
-        'main': {'temp': 273.15}
-    }
+    mock_get.return_value.json.return_value = None
 
-    api_key = 'test_api_key'
-    city = 'London'
+    url = "https://api.thecatapi.com/url_error"
+    cat_image_data = get_random_cat_image(url)
 
-    weather_data = get_weather(api_key, city)
-
-    assert weather_data == None
+    assert cat_image_data == None
